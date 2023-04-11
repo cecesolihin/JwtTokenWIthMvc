d604ccc96c91f3de4d8b3b6c4eee350c1f255c70
cecesolihin <49848882+cecesolihin@users.noreply.github.com>
2023-04-11 08:52:11 +0700

    go

---
+++ JWTAuth/JWTAuth/Repository/IChecklistRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JWTAuth.Models;
+
+
+namespace JWTAuth.Repository
+{
+    public interface IChecklistRepository
+    {
+        IList<Checklist> GetAllChecklists();
+        void Create(Checklist checklist);
+        void Update(Checklist checklist);
+        void Delete(int checklistId);
+
+        //Checklist Item
+        IList<ChecklistItem> GetAllChecklistsItemByChecklistId(int checklistId);
+        void CreateItemByChecklistId(Checklist checklist);
+        IList<ChecklistItem> GetChecklistsItemById(int checklistId, int checklistItemId);
+        void UpdateChecklistsItemByChecklistId(int checklistId, ChecklistItem checklistItem);
+        void DeleteChecklistsItemByChecklistId(int checklistId, int checklistItemId);
+        //void UpdateChecklistsItemByChecklistId(int checklistId, ChecklistItem checklistItem);
+
+    }
+}
