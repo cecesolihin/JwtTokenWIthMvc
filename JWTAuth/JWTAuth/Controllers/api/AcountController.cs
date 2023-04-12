083d092bbb2b9c2e0a1580d2949bf7263192c660
cecesolihin <49848882+cecesolihin@users.noreply.github.com>
2023-04-12 10:01:54 +0700

    User register

---
+++ JWTAuth/JWTAuth/Controllers/api/AcountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using JWTAuth.Models;
+using JWTAuth.Repository;
+
+namespace JWTAuth.Controllers.api
+{
+    public class AcountController : ApiController
+    {
+        private UserRepository repo = new UserRepository();
+        public AcountController()
+        {
+
+        }
+        [System.Web.Http.Route("~/api/register")]
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage Register(User user)
+        {
+            string productId = string.Empty;
+            HttpResponseMessage responseMessage = new HttpResponseMessage();
+            try
+            {
+                repo.CreateUser(user);
+                responseMessage = Request.CreateResponse(HttpStatusCode.OK, "success");
+            }
+            catch (System.Exception ex)
+            {
+
+
+                responseMessage = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return responseMessage;
+        }
+    }
+}
---
+++ JWTAuth/JWTAuth/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using JWTAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace JWTAuth.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public readonly string connectionString = ConfigurationManager.ConnectionStrings["connection"].ToString();
+        public void CreateUser(User user)
+        {
+            var sql = @"Insert Into [dbo].[User] values(@UserName,@Password,@Email)";
+
+            using (SqlConnection db = new SqlConnection(connectionString))
+            {
+                var exec = db.Execute(sql, new {user.UserName, user.Password,user.Email});
+            }
+        }
+    }
+}
